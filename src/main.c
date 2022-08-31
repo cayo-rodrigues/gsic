b3eba90a6d080969be1e7a8cdeb16eea6b2a99e5
cayo-rodrigues <cayo.rodrigues1914@gmail.com>
2022-08-31 10:18:56 -0300

    feat: succesfully open and close file + populate db

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+const char *PRODUCTS_DB_PATH = "src/db/products.csv";
 
 int main(int argc, char *argv[])
 {
-    printf("Everything working fine\n");
+    FILE *fp = fopen(PRODUCTS_DB_PATH, "r");
+    if (fp == NULL)
+    {
+        printf("Could not read file");
+        return 1;
+    }
+
+    fclose(fp);
 }
